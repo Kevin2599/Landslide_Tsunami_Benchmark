b2c793ebb583fd9535da7a891be9bf00500f0e6b
Roy Walters <rawamail@shaw.ca>
2017-11-14 20:13:11 -0800

    add SG1-IS5 without main data

---
+++ data/SG1_IS5/FullField/Plot_FullField_2D.m
@@ -0,0 +1,72 @@
+ChartTitle='Full Wave Field - SG1-IS5';            %3D plot title
+
+% Loads Position, Time, and Water Level variables from saved combined .mat
+% file
+open('PosAll_1_31.mat');
+Zall_Xmod=ans.Zall_Xmod;
+Zall_WL=ans.Zall_WL;
+Zall_T=ans.Zall_T;
+
+% Create new meshgrid with smaller time intervals and larger spatial
+% intervals and interpolate WL on this new grid to remove 'hairiness'.
+% Cubic used to interpolate
+[Position,Time] = meshgrid(0:3.99:10100,0:.006667:8.0004);
+WaterLevel = interp2(Zall_Xmod,Zall_T,Zall_WL,Position,Time,'cubic');
+
+% Reads in x position time history.  Slider block CoM location
+open('x_vs_time_fit.mat');
+x_vs_time_fit=ans.x_vs_time_fit;
+time=x_vs_time_fit.xi;
+x=x_vs_time_fit.yfit;
+
+% creates false 3rd dimesion to CoM position data to move it above the WL
+% plot
+for i=1:120;
+    y_false(i)=40;
+end
+% pads CoM x-position with last position value
+for i=91:120;
+    x(i)=x(90);
+end
+% pads time array with full 8sec
+for i=1:120;
+    time(i)=(i-1)*(1/15);
+end
+
+% Plots 2D WL profile
+figure(1)
+set(axes('FontSize', 14));
+colormap(jet)
+%contourf(Position,Time,WaterLevel,14);     % plots combined zone water level profile time history
+mesh(Position,Time,WaterLevel);
+colorbar;
+shading interp;
+
+hold on;
+plot3(x,time,y_false,'k o-');     % plots block CoM position on graph
+%plot(maxpos,Zall_T,'b');
+%plot(minpos,Zall_T,'r');
+hold off;
+
+hold on;
+run 'CharacteristicCurve_GroupVel';
+hold off;
+
+Title=title(ChartTitle);
+xlabel('distance from shore (mm)');
+ylabel('time (s)');
+%zlabel('Water level (mm)');
+%legend('Block CoM position',2)
+%legend('landslide CoM position',4)
+%legend('landslide CoM position','phase characteristic lambda=0.2m','group characteristic lambda=0.2m',...
+%    'phase characteristic lambda=0.5m','group characteristic lambda=0.5m',...
+%    'phase characteristic lambda=1m','group characteristic lambda=1m',...
+%    'phase characteristic lambda=2m','group characteristic lambda=2m',...
+%    'phase characteristic lambda=4m','group characteristic lambda=4m',...
+%    'phase characteristic lambda=6m','group characteristic lambda=6m',4)
+legend('landslide CoM position','phase characteristic lambda=0.15m','group characteristic lambda=0.15m',...
+    'phase characteristic lambda=4m','group characteristic lambda=4m',...
+    'phase characteristic lambda=6m','group characteristic lambda=6m',4)
+axis([-100 10200 0 8]);    % Fix up axes to fit image
+
+clear i m n
---
+++ data/SG1_IS5/timestep_0pt01/BottomProfile_part1.m
@@ -0,0 +1,165 @@
+% Generates bottom profile (boundary condition) time history
+
+clear
+
+% Loads x and y position, and u and v velocity, and time
+open('x_vs_time_fit_model.mat');
+x_vs_time_fit=ans.x_vs_time_fit_model;
+time=x_vs_time_fit.xi;
+x=x_vs_time_fit.yfit;
+x=x/1000;       %to convert from mm to m
+[I,J]=size(x);
+
+open('y_vs_time_fit_model.mat');
+y_vs_time_fit=ans.y_vs_time_fit_model;
+y=y_vs_time_fit.yfit;
+y=y/1000;       %to convert from mm to m
+
+
+for i=1:I;  % at each block position in each frame, 
+    K=(0.2800:-.0005:0.2100);   %trials trailing edge of block from 0.28m-0.21m behind measured block CoM 
+    for k=1:141;
+        Tx(i)=x(i)-K(k);
+            if Tx(i)<=1.297;    % at each trial for trailing edge x-pos, calculates TE y-pos
+                Ty(i)=-(Tx(i)*tan(15*pi/180));
+            elseif Tx(i)>=1.807;
+                Ty(i)=-0.435;
+            else 
+                Ty(i)=(0.19*((Tx(i)-1.297)^3))+(0.1024*((Tx(i)-1.297)^2))-(.2728*(Tx(i)-1.297))-0.3475;
+            end
+
+                if Tx(i)<=(1.297-0.5*cos(15*pi/180));
+                    Lx(i)=Tx(i)+0.500*cos(15*pi/180);   % for each trial for TE pos, calculates corresponding leading edge x- & y-pos
+                    Ly(i)=Ty(i)-0.500*sin(15*pi/180);
+                elseif Tx(i)>(1.297-0.5*cos(15*pi/180)) & Tx(i)<1.314;
+                    string=['((x-',num2str(Tx(i)),')^2)+((',num2str(Ty(i)),'-((0.19*((x-1.297)^3))+(0.1024*((x-1.297)^2))-(0.2728*(x-1.297))-0.3475))^2)-(0.500^2)'];
+                    Lx(i)=fzero(vectorize (inline (char (string))),1.8);
+                    Ly(i)=(0.19*((Lx(i)-1.297)^3))+(0.1024*((Lx(i)-1.297)^2))-(0.2728*(Lx(i)-1.297))-0.3475;
+                elseif Tx(i)>=1.314 & Tx(i)<1.807 
+                    Ly(i)=-0.435;
+                    Lx(i)=(sqrt((0.500^2)-((Ty(i)-Ly(i))^2)))+Tx(i);
+                elseif Tx(i)>=1.807;
+                    Lx(i)=Tx(i)+0.500;
+                    Ly(i)=-0.435;
+                else
+                    Lx(i)=0;
+                    Ly(i)=0;
+                end
+            
+            % at each trial of TE position, calculates corresponding block centre    
+            Cx(i)=(Tx(i)+((Lx(i)-Tx(i))/2))+(0.013*sin(atan((Ty(i)-Ly(i))/(Lx(i)-Tx(i)))));
+            Cy(i)=(Ly(i)+((Ty(i)-Ly(i))/2))+(0.013*cos(atan((Ty(i)-Ly(i))/(Lx(i)-Tx(i)))));
+         
+        % at each trial of TE position, compares calculated CoM position with measured CoM position    
+        %delta(k)=sqrt(((Cx(i)-x(i))^2)+((Cy(i)-y(i))^2));   % minimise absolute distance difference
+        delta(k)=sqrt(((Cy(i)-y(i))^2));                   % minimise vertical distance difference
+        [delta_min(i),Kmin(i)]=min(delta);  % finds at what trial for TE position provides the CoM closest to the measured CoM
+        
+        % calculate Tx,Ty,Lx,Ly,Cx,Cy at the optimal TE trial location
+        Tx(i)=x(i)-K(Kmin(i));
+            if Tx(i)<=1.297;    
+                Ty(i)=-(Tx(i)*tan(15*pi/180));
+            elseif Tx(i)>=1.807;
+                Ty(i)=-0.435;
+            else 
+                Ty(i)=(0.19*((Tx(i)-1.297)^3))+(0.1024*((Tx(i)-1.297)^2))-(.2728*(Tx(i)-1.297))-0.3475;
+            end
+
+                if Tx(i)<=(1.297-0.5*cos(15*pi/180));
+                    Lx(i)=Tx(i)+0.500*cos(15*pi/180);   % for each trial for TE pos, calculates corresponding leading edge x- & y-pos
+                    Ly(i)=Ty(i)-0.500*sin(15*pi/180);
+                elseif Tx(i)>(1.297-0.5*cos(15*pi/180)) & Tx(i)<1.314;
+                    string=['((x-',num2str(Tx(i)),')^2)+((',num2str(Ty(i)),'-((0.19*((x-1.297)^3))+(0.1024*((x-1.297)^2))-(0.2728*(x-1.297))-0.3475))^2)-(0.500^2)'];
+                    Lx(i)=fzero(vectorize (inline (char (string))),1.8);
+                    Ly(i)=(0.19*((Lx(i)-1.297)^3))+(0.1024*((Lx(i)-1.297)^2))-(0.2728*(Lx(i)-1.297))-0.3475;
+                elseif Tx(i)>=1.314 & Tx(i)<1.807 
+                    Ly(i)=-0.435;
+                    Lx(i)=(sqrt((0.500^2)-((Ty(i)-Ly(i))^2)))+Tx(i);
+                elseif Tx(i)>=1.807;
+                    Lx(i)=Tx(i)+0.500;
+                    Ly(i)=-0.435;
+                else
+                    Lx(i)=0;
+                    Ly(i)=0;
+                end
+        
+            % at the optimum TE position, calculates corresponding block CoM    
+            Cx(i)=(Tx(i)+((Lx(i)-Tx(i))/2))+(0.013*sin(atan((Ty(i)-Ly(i))/(Lx(i)-Tx(i)))));
+            Cy(i)=(Ly(i)+((Ty(i)-Ly(i))/2))+(0.013*cos(atan((Ty(i)-Ly(i))/(Lx(i)-Tx(i)))));
+            % at the optimum TE position, calculates corresponding origin of ellipse
+            Ox(i)=(Tx(i)+((Lx(i)-Tx(i))/2));
+            Oy(i)=(Ly(i)+((Ty(i)-Ly(i))/2));
+            % at the optimum TE position, calculates corresponding angle (degrees) of
+            % block to the horizontal
+            theta(i)=(atan((Ty(i)-Ly(i))/(Lx(i)-Tx(i))))/(pi/180);   
+        end 
+    
+    % at each time step (i), calculate the ellipse profile    
+    %e=(Tx(i):0.001:Lx(i));
+    %[K,L]=size(e);
+
+    %for j=1:L;
+    %    E_search_min(j)=(Ty(i)-((e(j)-Tx(i))*(Ty(i)-Ly(i))/(Lx(i)-Tx(i))));
+    %    E_search_max(j)=(E_search_min(j)+(0.026/cos(theta(i)*pi/-180))+0.000);
+    %    E_search=[E_search_max(j)];
+    
+    %    string=['(((((',num2str(e(j)),'-',num2str(Ox(i)),')*cos(',num2str(theta(i)*pi/-180),'))+((y-',num2str(Oy(i)),')*sin(',num2str(theta(i)*pi/-180),')))^2)/(0.25^2))+(((((y-',num2str(Oy(i)),')*cos(',num2str(theta(i)*pi/-180),'))-((',num2str(e(j)),'-',num2str(Ox(i)),')*sin(',num2str(theta(i)*pi/-180),')))^2)/(0.026^2))-1'];
+    %    E(i,j)=fzero(vectorize (inline (char (string))),E_search);
+    %end
+        
+    i=i+1
+end
+
+clear I J K Kmin ans delta i k string x_vs_time_fit y_vs_time_fit
+
+figure(1)
+plot(x,time,'b');
+hold on;
+plot(Cx,time,'r');
+hold off;
+title('Comparison of Calculated CoM x-position');
+xlabel('distance from shore (m)');
+ylabel('time (s)');
+axis([0 6 0 6]);
+grid on;
+legend('measured','calculated',1)
+
+figure(2)
+plot(y,time,'b');
+hold on;
+plot(Cy,time,'r');
+hold off;
+title('Comparison of Calculated CoM y-position');
+xlabel('distance from still water level (m)');
+ylabel('time (s)');
+axis([-0.5 0 0 6]);
+grid on;
+legend('measured','calculated',1)
+
+figure(3)
+plot(x,y,'b');
+hold on;
+plot(Cx,Cy,'r');
+hold off;
+title('Comparison of Calculated CoM Position');
+xlabel('distance from shore (m)');
+ylabel('distance from still water level (m)');
+axis([0 6 -0.5 0]);
+grid on;
+legend('measured','calculated',1)
+
+figure(4)
+plot(time,delta_min,'b');
+title('Absolute Distance Between Measured and Calculated CoM Position');
+xlabel('time (s)');
+ylabel('absolute distance between measured and calculated CoM position (m)');
+axis([0 6 0 0.02]);
+grid on;
+
+figure(5)
+plot(time,theta,'b');
+title('Block Angle Relative to Horizontal');
+xlabel('time (s)');
+ylabel('block angle relative to horizontal (degrees)');
+axis([0 6 0 16]);
+grid on;
